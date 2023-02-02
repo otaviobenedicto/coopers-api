90ac34034e2b426c6dbaa1bbf88a6a08de26bdc5
otaviobenedicto <otavio.bene@outlook.com>
2023-02-02 18:46:47 -0300

    Porta alterada

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const connect = async () => {
 app.use("/todo",ToDoRoutes)
 app.use("/users", AuthRoutes)
 
-const PORT = process.env.PORT | 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
     connect()
